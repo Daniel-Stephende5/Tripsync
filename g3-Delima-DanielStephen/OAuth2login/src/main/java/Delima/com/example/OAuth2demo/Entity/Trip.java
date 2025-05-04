03ce14e8360321536854371c0926b0d569226acb
Daniel-Stephende5 <danielstephen.delima@cit.edu>
2025-05-04 11:34:23 +0800

    Update Trip.java

--- g3-Delima-DanielStephen/OAuth2login/src/main/java/Delima/com/example/OAuth2demo/Entity/Trip.java
+++ g3-Delima-DanielStephen/OAuth2login/src/main/java/Delima/com/example/OAuth2demo/Entity/Trip.java
@@ -16,7 +16,9 @@ public class Trip {
 
     private double originLat;
     private double originLon;
-
+    @ManyToOne
+@JoinColumn(name = "user_id", nullable = false)
+private User user;
     private LocalDate travelDate;
     @Lob
     private String mapImage;
