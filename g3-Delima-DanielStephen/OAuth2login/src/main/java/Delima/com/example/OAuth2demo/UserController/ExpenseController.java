a9d35aeb07cfae1b5881f382aa37cba077bf1955
Daniel-Stephende5 <danielstephen.delima@cit.edu>
2025-05-07 23:28:07 +0800

    Update ExpenseController.java

--- g3-Delima-DanielStephen/OAuth2login/src/main/java/Delima/com/example/OAuth2demo/UserController/ExpenseController.java
+++ g3-Delima-DanielStephen/OAuth2login/src/main/java/Delima/com/example/OAuth2demo/UserController/ExpenseController.java
@@ -14,7 +14,7 @@ import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/api/expenses")
-@CrossOrigin(origins = "http://localhost:3000")
+
 public class ExpenseController {
 
     private final ExpenseService expenseService;
